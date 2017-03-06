0a5033cf6d4b6309e07e2b6458688f98a8853cb8
JacobLondon <jelondon12@gmail.com>
2017-03-05 19:45:21 -0800

    Activity Plan Table Model Progress
    Set up the time string vector

--- src/main/java/Graphics/OptionsMenu/ActivityPlanModel/ActivityPlanTableModel.java
+++ src/main/java/Graphics/OptionsMenu/ActivityPlanModel/ActivityPlanTableModel.java
@@ -1,5 +1,6 @@
 package Graphics.OptionsMenu.ActivityPlanModel;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Vector;
 
 import javax.swing.JComboBox;
@@ -13,13 +14,25 @@ public class ActivityPlanTableModel extends AbstractTableModel {
 	private Data data;
 	private Vector<Vector<JComboBox>> tableData;
 	private Vector<String> columnNames;
+	private Vector<String> timeVector;
+	private Preset preset;
 	
 	public ActivityPlanTableModel(Preset preset){
+		
+		this.preset = preset;
 		
 		// number of rows
 		tableData = new Vector<Vector<JComboBox>>(preset.getSlotNumber());
 		// number of columns
 		columnNames = new Vector<String>(data.getGroupList().size() + 1);
+		
+		timeVector = new Vector<String>(preset.getSlotNumber());
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh.mm a");
+		for(int count = 0; count < preset.getSlotNumber(); count++){
+			timeVector.add(count, preset.getStartTime().plusMinutes(preset.getIntervalTime() * (count)).format(formatter) +
+					" - " + preset.getStartTime().plusMinutes(preset.getIntervalTime() * (count + 1)).format(formatter));
+		}
 		
 		// populate array for combo box selections
 		String[] activityNameArray = new String[data.getPlannedActivityList().size() + 1];
@@ -40,17 +53,21 @@ public class ActivityPlanTableModel extends AbstractTableModel {
 	}
 	
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return data.getGroupList().size() + 1;
 	}
 
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return preset.getSlotNumber();
 	}
 
-	public Object getValueAt(int arg0, int arg1) {
-		// TODO Auto-generated method stub
+	public Object getValueAt(int row, int column) {
+		if(column == 0){
+			return preset.getStartTime();
+		}
+		if(column == 1){
+			return tableData.get(row).get(column - 1);
+		}
+		
 		return null;
 	}
 
