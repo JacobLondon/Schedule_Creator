835f3ea5db6fe14cd9e8bdbae34be1df9f509ee3
JacobLondon <jelondon12@gmail.com>
2017-06-14 17:58:05 -0700

    Several Methods for FillActivity
    Worked on several helper methods for FillActivity

--- src/main/java/Graphics/FillActivity.java
+++ src/main/java/Graphics/FillActivity.java
@@ -10,29 +10,48 @@ import Resources.Data;
 public class FillActivity {
 
 	private String[][] outputArray;
+	
+	// elements are stored here when they are temporarily removed from presetScheduleForEachGroup
 	private ArrayList<String> randomActivityPerRow = new ArrayList<String>();
 	private Preset preset;
 	private ArrayList<ArrayList<String>> presetSchedule;
+	private ArrayList<ArrayList<String>> presetScheduleCopy;
 	private ArrayList<RandomActivity> randomActivityList = Data.getData().getRandomActivityList();
 	private ArrayList<PlannedActivity> plannedActivityList = Data.getData().getPlannedActivityList();
 	
 	private ArrayList<ArrayList<String>> presetScheduleForEachGroup;
 	
+	private int totalNumSubGroups = 0;
+	
 	public FillActivity(Preset preset){
 		this.preset = preset;
-		presetScheduleForEachGroup = createGroupListForRandomActivities();
+		presetSchedule = preset.getPresetSchedule();
+		refillElements();
+		
+		for(int i = 0; i < preset.getGroupList().size(); i++){
+			totalNumSubGroups += preset.getGroupList().get(i).getSubGroupCount();
+		}
+		
+		// Make a deep copy of preset schedule
+		for(int i = 0; i < presetSchedule.size(); i++){
+			presetScheduleCopy.add(new ArrayList<String>());
+			for(int j = 0; j < presetSchedule.get(i).size(); i++){
+				presetScheduleCopy.get(i).add(presetSchedule.get(i).get(j));
+			}
+		}
 		
 	}
 	
 	public void fillActivity(){
 		
-		
-		for(int i = 0; i < presetSchedule.size(); i++){
-			for(int j = 0; j < presetSchedule.get(0).size(); j++){
-				if(presetSchedule.get(i).get(j).equals("Random")){
+		for(int i = 0; i < preset.getSlotNumber(); i++){
+			 
+			// each row
+			for(int j = 0; j < totalNumSubGroups; j++){
+				if(presetScheduleCopy.get(i).get(j).equals("Random")){
 					
 				}
-				else if(!presetSchedule.get(i).get(j).equals("Random")){
+				else if(!presetScheduleCopy.get(i).get(j).equals("Random")){
 					//not random, but planned
 				}
 			}
@@ -40,6 +59,11 @@ public class FillActivity {
 		
 	}
 	
+	/**
+	 *  Makes a 2D arraylist of strings where each sub-arraylist contains the random activities
+	 *  each group can have respectively.
+	 * @return 2D ArrayList of Strings with RandomActivity names
+	 */
 	private ArrayList<ArrayList<String>> createGroupListForRandomActivities(){
 		ArrayList<ArrayList<String>> outputList = new ArrayList<ArrayList<String>>();
 		
@@ -52,5 +76,42 @@ public class FillActivity {
 			}
 		}
 		return outputList;
+	}
+	
+	/**
+	 * Removes the given String from the 2D ArrayList of Strings in each ArrayList
+	 * Adds the removed element into another ArrayList to keep track of elements.
+	 * @param element The String to be removed from the ArrayList.
+	 */
+	private void removeElement(String element){
+		for(int i = 0; i < presetScheduleForEachGroup.size(); i++){
+			for(int j = 0; j < presetScheduleForEachGroup.get(i).size(); j++){
+				if(presetScheduleForEachGroup.get(i).get(j).equals(element)){
+					presetScheduleForEachGroup.get(i).remove(j);
+				}
+			}
+		}
+	}
+		
+	private void refillElements(){
+		presetScheduleForEachGroup = createGroupListForRandomActivities();
+	}
+	
+	/**
+	 * Finds and returns the Group number which specifies the Group from the total amount of subgroups.
+	 * @param groupNum The group number with all subgroups accounted for from previous Groups
+	 * @return The equivalent Group index in GroupList 
+	 */
+	private int findGroup(int groupNum){
+		for(int i = 0; i < preset.getGroupList().size(); i++){
+			if(groupNum - preset.getGroupList().get(i).getSubGroupCount() >= 0){
+				groupNum -= preset.getGroupList().get(i).getSubGroupCount();
+			}
+			else{
+				return groupNum;
+			}
+			
+		}
+		
 	}
 }
